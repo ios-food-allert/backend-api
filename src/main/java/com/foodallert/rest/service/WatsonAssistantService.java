1937d34dd7594b8154660328e06b1db795846936
josegilramirez <josegil_ramirez@sps.com.mx>
2019-01-29 18:47:09 -0600

    C:/Program Files/Git/getArticlesList endpoint added

--- src/main/java/com/foodallert/rest/service/WatsonAssistantService.java
+++ src/main/java/com/foodallert/rest/service/WatsonAssistantService.java
@@ -3,7 +3,11 @@
  */
 package com.foodallert.rest.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,8 +15,10 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
 
+import com.foodallert.dto.Article;
 import com.foodallert.dto.AssistantRequest;
 import com.foodallert.extension.FoodAllertMessageResponse;
+import com.foodallert.util.ArticlesGenerator;
 import com.foodallert.watson.services.AssistantConnector;
 
 /**
@@ -36,5 +42,17 @@ public class WatsonAssistantService {
 		response = connector.sendAssistantRequest(request);
 
 		return response;
+	}
+	
+	@GET
+	@Path("/getArticlesList")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Article> getArticlesList() {
+		logger.info("GET /getArticlesList");
+		List<Article> articles = new ArrayList<Article>();
+		
+		articles = ArticlesGenerator.generateArticlesList();
+		
+		return articles;
 	}
 }
